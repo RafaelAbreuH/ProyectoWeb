38bd798846a36575c8f2c56e681d7bfe63b7d49f
Rafael Abreu Hernandez <rafael_abreu5@ucne.edu.do>
2019-09-02 11:36:40 -0400

    Arreglando

--- ProyectoWeb/Properties/AssemblyInfo.cs
+++ ProyectoWeb/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("870c40de-0e44-4388-be0b-a3982b155b3e")]
+[assembly: Guid("af49edc1-1ff4-4ee6-8bce-e70e659a4f4a")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ ProyectoWeb/Registro/rUsuario.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProyectoWeb.Registro
+{
+    public partial class rUsuario : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BuscarButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void nuevoButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void guadarButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void eliminarButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
